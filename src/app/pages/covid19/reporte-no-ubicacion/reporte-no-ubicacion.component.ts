90e0e14e50b12e11d0c953ca82ca11e9d47c2f51
Christian Delgado <christian.delgadopy@gmail.com>
2020-04-18 12:22:15 -0400

    Filtro por tipo de paciente seleccionando el filtro tipo de paciente
    Signed-off-by: Christian Delgado <christian.delgadopy@gmail.com>

--- src/app/pages/covid19/reporte-no-ubicacion/reporte-no-ubicacion.component.ts
+++ src/app/pages/covid19/reporte-no-ubicacion/reporte-no-ubicacion.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Message, ConfirmationService } from 'primeng/api';
+import {Message, ConfirmationService, SelectItem} from 'primeng/api';
 import { FormControl, Validators, FormGroup } from '@angular/forms';
 import {ReporteNoUbicacionModel} from "./model/reporte-no-ubicacion.model";
 import {PermissionGuardService} from "../../../services/permission-guard.service";
@@ -20,7 +20,7 @@ export class ReporteNoUbicacionComponent implements OnInit, OnDestroy {
   cols: any[];
   pageSize: number = 10;
   start: number = 0;
-  filter: string;
+  search: string;
   totalRecords: number = 0;
   sortDesc: boolean = true;
   sortField: string;
@@ -29,6 +29,9 @@ export class ReporteNoUbicacionComponent implements OnInit, OnDestroy {
   loading = true;
   error = false;
   private loadSubsciption: Subscription;
+  filters: SelectItem[];
+  selectedFilter: SelectItem;
+  filter: string;
 
   constructor(private _reporteService: ReporteNoUbicacionService, private permission: PermissionGuardService, private datepipe: DatePipe) { }
 
@@ -37,6 +40,10 @@ export class ReporteNoUbicacionComponent implements OnInit, OnDestroy {
       { field: 'nombreCompleto', header: 'Nombre', width: '40%' },
       { field: 'cedula', header: 'Cédula', width: '20%' },
       { field: 'fechaUltimoReporte', header: 'Fecha Último Reporte', width: '40%', isDate: true, sort: false, fieldEntity: 'fechaUltimoReporteUbicacion' },
+    ];
+    this.filters = [
+      {label: 'Seleccione filtro', value: null},
+      {label: 'Tipo de Paciente', value: 'resultadoUltimoDiagnostico'},
     ];
   }
 
@@ -47,12 +54,17 @@ export class ReporteNoUbicacionComponent implements OnInit, OnDestroy {
   load($event: any) {
 
     if ($event) {
-      this.filter = $event.globalFilter;
+      this.search = $event.globalFilter;
+      this.filter = null;
       this.pageSize = $event.rows;
       this.start = $event.first / this.pageSize;
       let field = this.cols.find(c => c.field === $event.sortField);
       this.sortField = (field ? field.fieldEntity : field) || $event.sortField;
       this.sortDesc = $event.sortOrder == -1;
+      if(this.selectedFilter && $event.globalFilter) {
+        this.filter = `${this.selectedFilter}:${$event.globalFilter}`;
+        this.search = null;
+      }
     }
 
     this.loadReporte();
@@ -65,7 +77,7 @@ export class ReporteNoUbicacionComponent implements OnInit, OnDestroy {
     if (this.loadSubsciption && !this.loadSubsciption.closed) {
       this.loadSubsciption.unsubscribe();
     }
-    this.loadSubsciption = this._reporteService.getAllQueryReporte(this.start, this.pageSize, this.filter, this.sortDesc, this.sortField).subscribe(res => {
+    this.loadSubsciption = this._reporteService.getAllQueryReporte(this.start, this.pageSize, this.search, this.sortDesc, this.sortField, this.filter).subscribe(res => {
       if(res.status === 200){
         this.reportes = res.body;
         this.totalRecords = res.headers.get('x-total-count');
@@ -82,7 +94,7 @@ export class ReporteNoUbicacionComponent implements OnInit, OnDestroy {
 
   onDownloadCsv() {
     this.loading = true;
-    this._reporteService.downloadCSV(this.filter, this.sortDesc, this.sortField).subscribe(
+    this._reporteService.downloadCSV(this.search, this.sortDesc, this.sortField).subscribe(
       (data: any) => {
         let latest_date = this.datepipe.transform(new Date(), 'yyyyMMddHHmmss');
         saveAs(data,`ListaPacientes${latest_date}.csv`);
