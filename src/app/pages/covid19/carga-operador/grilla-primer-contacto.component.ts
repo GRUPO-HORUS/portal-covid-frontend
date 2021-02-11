0d09a2e3cfc6a6139c7f42831ea72968c1ef8427
dalvarez26 <diego.alvarez@horustek.com>
2021-02-11 13:23:57 -0300

    Cambios

--- src/app/pages/covid19/carga-operador/grilla-primer-contacto.component.ts
+++ src/app/pages/covid19/carga-operador/grilla-primer-contacto.component.ts
@@ -1014,7 +1014,7 @@ consultarIdentificaciones(event) {
         this.openMessageDialog();
         this.primerContacto.editado = true;
 
-        this.service.insertFrmFsarscov2(this.primerContacto).subscribe(response => {
+        /*this.service.insertFrmFsarscov2(this.primerContacto).subscribe(response => {
           
         }, error => {
           if(error.status == 401){
@@ -1025,7 +1025,7 @@ consultarIdentificaciones(event) {
             this.mensaje = error.error;
             this.openMessageDialog();
           }
-        });
+        });*/
 
     }, error => {
         if(error.status == 401)
