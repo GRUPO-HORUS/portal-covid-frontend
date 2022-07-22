94fb3f576614354ad316a31025185f209a126cf5
Ricardo Ramírez <5245542+RicardoBeltranRamirez@users.noreply.github.com>
2022-07-21 22:51:20 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiPortalPluginUrl: "APIURL",
-  googleReCaptchaApiKey : "6LdmkAwTAAAAABUO33kW8azJwwch7GbPnOISR6fu"
+  googleReCaptchaApiKey : "test"
 };
